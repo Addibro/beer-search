46177f2a460bf07b388d7352364daf4389257804
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-03-13 00:18:26 +0100

    Update ProductSearch.java

--- client/src/se/itu/systemet/main/ProductSearch.java
+++ client/src/se/itu/systemet/main/ProductSearch.java
@@ -4,17 +4,6 @@ import se.itu.systemet.gui.SearchGUI;
 
 public class ProductSearch {
   public static void main(String[] args) {
-//    fixLookAndFeel();
     javax.swing.SwingUtilities.invokeLater(SearchGUI::new);
   }
-  // For Rikard's computer - you can ignore this
-//  private static void fixLookAndFeel() {
-//    try {
-//      // Ignore the lines below - it's a fix for Rikard's computer. Hell Dell!
-//      javax.swing.UIManager
-//        .setLookAndFeel((javax.swing.LookAndFeel)Class
-//                        .forName("com.sun.java.swing.plaf.gtk.GTKLookAndFeel")
-//                        .newInstance());
-//    } catch (Exception ignore) {}
-//  }
 }
